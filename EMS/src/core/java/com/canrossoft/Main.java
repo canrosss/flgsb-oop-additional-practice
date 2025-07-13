01dce65d6d5b152506676f0bace38ed9b635820a
charlie <canros.mx@gmail.com>
2025-07-12 22:45:40 -0600

    Adding the first clasess

--- EMS/src/core/java/com/canrossoft/Main.java
+++ EMS/src/core/java/com/canrossoft/Main.java
@@ -1,17 +1,12 @@
 package com.canrossoft;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.canrossoft.model.Employee;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    protected static Employee e = new Employee("i", 19, 2000);
+    public static void main(String[] args) {
+        System.out.println("Welcome to EMS! - v0.1");
+        System.out.println(e.toString());
+
     }
 }
--- EMS/src/core/java/com/canrossoft/model/Employee.java
+++ EMS/src/core/java/com/canrossoft/model/Employee.java
@@ -1,4 +1,53 @@
 package com.canrossoft.model;
 
 public class Employee {
+    private String name;
+    private int age;
+    private double salary;
+
+    public Employee(){
+        this.name = "Unknow";
+        this.age = 18;
+        this.salary = 0.0;
+    }
+
+    public Employee(String name, int age, double salary){
+        setName(name);
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        if (!name.isEmpty()) {
+            this.name = name;
+        } else{
+            throw new IllegalArgumentException("Name cannot be empty!");
+        }
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString(){
+        return "Employee {Name '" + this.name + "', age=" + this.age + ", salary=" + this.salary + "}";
+
+    }
 }
